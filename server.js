3cad325fc5eaef1202fe07051ac5a27ae1844a31
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-04-21 10:57:09 -0400

    application initialized and installed some package as json and cors

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotEnv = require("dotenv");
+
+dotEnv.config();
+
+const app = express();
+
+// const middleware = (req, res, next) => {
+//     console.log("Hey wassup");
+//     next();
+// };
+
+// Request payload middleware for json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => {
+    res.send("Hello from nodejs api 2 server");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+// Error handler middleware
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send({
+        status: 500,
+        message: err.message,
+        body: {},
+    });
+});

dc6a6aee3821538ee90e6138917a47e8f5ad3d41
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-04-21 12:07:30 -0400

    new routes for post a new products on database

---
+++ database/models/productModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+    {
+        name: String,
+        price: Number,
+        brand: String,
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/v1/product", require("./routes/productRoutes"));
+
 app.get("/", (req, res, next) => {
     res.send("Hello from nodejs api 2 server");
 });
